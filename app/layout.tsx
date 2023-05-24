dfe697983c0442207b600c4f6fbd876fccb53581
nordowl <jonas@nordstudio.digital>
2023-05-24 13:11:25 +0200

    Add robots metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,11 @@ import Navigation from "@/src/components/Navigation";
 import { headingFont, bodyFont } from "@/src/fonts";
 import clsx from "clsx";
 import "@/src/styles/__styles.scss"
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    robots: { index: false, follow: false }
+}
 
 const RootLayout = ({children}: {children: ReactNode}) => {
     return (

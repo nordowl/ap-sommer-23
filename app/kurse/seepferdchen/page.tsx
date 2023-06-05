4981ca40adb735a83a16478128ee27ff9a15f233
nordowl <jonas@nordstudio.digital>
2023-06-05 11:49:16 +0200

    Add missing courses for Seepferdchen

--- app/kurse/seepferdchen/page.tsx
+++ app/kurse/seepferdchen/page.tsx
@@ -39,131 +39,175 @@ const Page = () => {
                                     day: 6,
                                     info: "09:30 - 10:15 (45 Minuten)",
                                     isStart: true,
-                                    onClickDay: ()=> undefined,
                                     border: "blue"
                                 },
                                 {
                                     day: 7,
                                     info: "09:30 - 10:15 (45 Minuten)",
-                                    onClickDay: ()=> undefined,
                                     border: "blue"
                                 },
                                 {
                                     day: 8,
                                     info: "09:30 - 10:15 (45 Minuten)",
-                                    onClickDay: ()=> undefined,
                                     border: "blue"
                                 },
                                 {
                                     day: 9,
                                     info: "09:30 - 10:15 (45 Minuten)",
-                                    onClickDay: ()=> undefined,
                                     border: "blue"
                                 },
                                 {
                                     day: 10,
                                     info: "09:30 - 10:15 (45 Minuten)",
-                                    onClickDay: ()=> undefined,
                                     border: "blue"
                                 },
                                 {
                                     day: 11,
                                     info: "09:30 - 10:15 (45 Minuten)",
-                                    onClickDay: ()=> undefined,
                                     border: "blue"
                                 },
                                 {
                                     day: 12,
                                     info: "09:30 - 10:15 (45 Minuten)",
-                                    onClickDay: ()=> undefined,
                                     border: "blue"
                                 },
                                 {
                                     day: 13,
                                     info: "09:30 - 10:15 (45 Minuten)",
-                                    onClickDay: ()=> undefined,
                                     border: "blue"
                                 },
                                 {
                                     day: 14,
                                     info: "09:30 - 10:15 (45 Minuten)",
-                                    onClickDay: ()=> undefined,
                                     border: "blue"
                                 },
                                 {
                                     day: 15,
                                     info: "09:30 - 10:15 (45 Minuten)",
                                     isEnd: true,
-                                    onClickDay: ()=> undefined,
                                     border: "blue"
                                 },
                                 {
                                     day: 20,
                                     info: "09:30 - 10:15 (45 Minuten)",
                                     isStart: true,
-                                    onClickDay: ()=> undefined,
                                     border: "orange"
                                 },
                                 {
                                     day: 21,
                                     info: "09:30 - 10:15 (45 Minuten)",
-                                    onClickDay: ()=> undefined,
                                     border: "orange"
                                 },
                                 {
                                     day: 22,
                                     info: "09:30 - 10:15 (45 Minuten)",
-                                    onClickDay: ()=> undefined,
                                     border: "orange"
                                 },
                                 {
                                     day: 23,
                                     info: "09:30 - 10:15 (45 Minuten)",
-                                    onClickDay: ()=> undefined,
                                     border: "orange"
                                 },
                                 {
                                     day: 24,
                                     info: "09:30 - 10:15 (45 Minuten)",
-                                    onClickDay: ()=> undefined,
                                     border: "orange"
                                 },
                                 {
                                     day: 25,
                                     info: "09:30 - 10:15 (45 Minuten)",
-                                    onClickDay: ()=> undefined,
                                     border: "orange"
                                 },
                                 {
                                     day: 26,
                                     info: "09:30 - 10:15 (45 Minuten)",
-                                    onClickDay: ()=> undefined,
                                     border: "orange"
                                 },
                                 {
                                     day: 27,
                                     info: "09:30 - 10:15 (45 Minuten)",
-                                    onClickDay: ()=> undefined,
                                     border: "orange"
                                 },
                                 {
                                     day: 28,
                                     info: "09:30 - 10:15 (45 Minuten)",
-                                    onClickDay: ()=> undefined,
                                     border: "orange"
                                 },
                                 {
                                     day: 29,
                                     info: "09:30 - 10:15 (45 Minuten)",
                                     isEnd: true,
-                                    onClickDay: ()=> undefined,
                                     border: "orange"
                                 },
                                 {
                                     day: 31,
                                     info: "09:30 - 10:15 (45 Minuten)",
                                     isEnd: true,
-                                    onClickDay: ()=> undefined,
+                                    border: "yellow"
+                                },
+                            ]
+                        },
+                        {
+                            name: "August",
+                            range: 31,
+                            skipFirst: 1,
+                            bookableDays: [
+                                {
+                                    day: 1,
+                                    info: "09:30 - 10:15 (45 Minuten)",
+                                    isStart: true,
+                                    border: "yellow"
+                                },
+                                {
+                                    day: 2,
+                                    info: "09:30 - 10:15 (45 Minuten)",
+                                    border: "yellow"
+                                },
+                                {
+                                    day: 3,
+                                    info: "09:30 - 10:15 (45 Minuten)",
+                                    border: "yellow"
+                                },
+                                {
+                                    day: 4,
+                                    info: "09:30 - 10:15 (45 Minuten)",
+                                    border: "yellow"
+                                },
+                                {
+                                    day: 5,
+                                    info: "09:30 - 10:15 (45 Minuten)",
+                                    border: "yellow"
+                                },
+                                {
+                                    day: 6,
+                                    info: "09:30 - 10:15 (45 Minuten)",
+                                    border: "yellow"
+                                },
+                                {
+                                    day: 7,
+                                    info: "09:30 - 10:15 (45 Minuten)",
+                                    border: "yellow"
+                                },
+                                {
+                                    day: 8,
+                                    info: "09:30 - 10:15 (45 Minuten)",
+                                    border: "yellow"
+                                },
+                                {
+                                    day: 9,
+                                    info: "09:30 - 10:15 (45 Minuten)",
+                                    border: "yellow"
+                                },
+                                {
+                                    day: 10,
+                                    info: "09:30 - 10:15 (45 Minuten)",
+                                    isEnd: true,
+                                    border: "yellow"
+                                },
+                                {
+                                    day: 11,
+                                    info: "09:30 - 10:15 (45 Minuten)",
+                                    isStart: true,
                                     border: "yellow"
                                 },
                             ]

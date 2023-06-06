8e36cadd19fca2c415e8682c0b782013ca5c0662
nordowl <jonas@nordstudio.digital>
2023-06-06 15:08:22 +0200

    Add Impressum & Kontakt

---
+++ app/impressum/page.tsx
@@ -0,0 +1,24 @@
+const Page = () => {
+    return (
+        <section className="narrow">
+            <h1 className="mb-8">Impressum</h1>
+            <p>Schwimmspaß GmbH</p>
+            <p>Badstraße<br/>
+                550555 Wasserland
+            </p>
+            <p>Tel: 0011/2200-0</p>
+            <p>Fax: 0011/2200-5</p>
+            <p><a href="https://www.schwimmspass-wasserland.de" className="link" target="blank">www.schwimmspass-wasserland.de</a></p>
+            <p>
+                Gesellschafterin: Stadtwerke Wasserland<br/>
+                Geschäftsführerin: Inka Möglich<br/>
+                Registergericht<br/>
+                Hannover GnR60-666<br/>
+                Umsatzsteuer-Identifikationsnummer<br/>
+                DE707777700
+            </p>
+        </section>
+    );
+}
+ 
+export default Page;
---
+++ app/kontakt/page.tsx
@@ -0,0 +1,15 @@
+const Page = () => {
+    return (
+        <section className="narrow">
+            <h1 className="mb-8">Kontakt</h1>
+            <p>Schwimmspaß GmbH</p>
+            <p>Badstraße<br/>
+                550555 Wasserland
+            </p>
+            <p>Tel: 0011/2200-0</p>
+            <p><a href="mailto:info@schwimmspass-wasserland.de" className="link">info@schwimmspass-wasserland.de</a></p>
+        </section>
+    );
+}
+ 
+export default Page;

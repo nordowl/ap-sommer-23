2a20efa38085ac3f296957d7e355ca09fb7b75c4
nordowl <jonas@nordstudio.digital>
2023-06-06 15:10:57 +0200

    Fix link

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ const Page = () => {
                             <p>Wir betreiben ein öffentliches <b>Schwimmbad für die ganze Familie</b>.</p>
                             <p>Egal, ob Sie mit Ihrem Kleinkind kommen, Jugendliche/r sind oder Sportschwimmer/-in, unsere Becken bieten allen einen passenden Ort. Dazu kommen Sprungtürme im Innen- und Außenbereich.</p>
                             <p>Über die <b>Sauna</b> erfahren Sie alles Wichtige hier: <a href="http://www.sauna-wasserland.de" className="link">www.sauna-wasserland.de</a>.</p>
-                            <p>Das Freibad mit Liegewiese ist <a href="" className="link">von Mai bis Oktober</a> geöffnet. Wir bieten außerdem: <a href="/angebote-events" className="link">Schwimmkurse für Kinder und Erwachsene</a>, <a href="/kurse/fitness" className="link">Aqua-Fitness-Angebote</a> sowie <a href="/angebote-events" className="link">spezielle Events für Groß und Klein</a>. Bleiben Sie neugierig!</p>
+                            <p>Das Freibad mit Liegewiese ist <a href="" className="link">von Mai bis Oktober</a> geöffnet. Wir bieten außerdem: <a href="/angebote-events" className="link">Schwimmkurse für Kinder und Erwachsene</a>, <a href="/kurse/aqua-fitness" className="link">Aqua-Fitness-Angebote</a> sowie <a href="/angebote-events" className="link">spezielle Events für Groß und Klein</a>. Bleiben Sie neugierig!</p>
                         </div>
                     </div>
                 </section>
